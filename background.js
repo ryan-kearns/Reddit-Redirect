9f0480971107ab75c2d65ed4cf481b22aad21ef2
Ryan J. Kearns <ryan@Ryans-MacBook-Pro.local>
2018-06-04 14:51:55 -0400

    forked from skearns4/Smile-Redirect -- updated onBeforeRequest listener to match reddit urls

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        return { redirectUrl: "https://smile.amazon.com/" + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1] };
+        return { redirectUrl: "https://old.reddit.com/" + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1] };
     },
     {
         urls: [
-            "*://amazon.com/*",
-            "*://www.amazon.com/*",
-            "*://amazon.co.uk/*",
-            "*://www.amazon.co.uk/*",
-            "*://amazon.ca/*",
-            "*://www.amazon.ca/*"
+            "*://reddit.com/*",
+            "*://www.reddit.com/*",
+            "*://reddit.co.uk/*",
+            "*://www.reddit.co.uk/*",
+            "*://reddit.ca/*",
+            "*://www.reddit.ca/*"
             ],
         types: [
             "main_frame",
